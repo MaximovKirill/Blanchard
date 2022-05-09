8d12c22443352ce2d2821082c07e6f26ed78f552
Kirill Maximov <maximov.kirill.alexandrovich@gmail.com>
2022-05-09 17:14:51 +0300

    header fixed scroll

--- js/index.js
+++ js/index.js
@@ -1,15 +1,26 @@
-// burger-menu
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#header__burger').addEventListener('click', function() {
-    document.querySelector('#header__nav-adaptive').classList.add('active');
-  });
-  document.querySelector('#nav-adaptive__close').addEventListener('click', function() {
-    document.querySelector('#header__nav-adaptive').classList.remove('active');
-  });
-  document.querySelectorAll('.header__link-adaptive').forEach(function(t) {
-    t.addEventListener('click', function(event) {
-      document.querySelector('#header__nav-adaptive').classList.remove('active');
-    });
-  });
+// swiper
+const swiper = new Swiper('.swiper', {
+  allowTouchMove: false,
+  loop: true,
+  effect: 'fade',
+  speed: 10000,
+  autoplay: {
+    delay: 10000,
+  },
 });
+
+// // burger-menu
+// window.addEventListener('DOMContentLoaded', function() {
+//   document.querySelector('#header__burger').addEventListener('click', function() {
+//     document.querySelector('#header__nav-adaptive').classList.add('active');
+//   });
+//   document.querySelector('#nav-adaptive__close').addEventListener('click', function() {
+//     document.querySelector('#header__nav-adaptive').classList.remove('active');
+//   });
+//   document.querySelectorAll('.header__link-adaptive').forEach(function(t) {
+//     t.addEventListener('click', function(event) {
+//       document.querySelector('#header__nav-adaptive').classList.remove('active');
+//     });
+//   });
+// });
 
